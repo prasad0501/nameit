e98dad2e330bf72b2d49084ffec03c2dffdc3746
N Madhusudan Nayak <prasad.nayak60@gmail.com>
2021-04-26 23:03:42 +0530

    added image files

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Header.css";
+import nameitpic from "../../assets/nameit.png";
 const Header = (props) => {
   return (
     <div className="head-container">
       <img
-        src="https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png"
+        src={nameitpic}
         className={`head-image ${
           props.headerExpanded ? "head-image-expanded" : "head-image-contracted"
         }`}
